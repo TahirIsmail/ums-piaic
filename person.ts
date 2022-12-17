e3e480d5ab7910204ba658e3b550b8824c78dcfb
TahirIsmail <vjtary104@gmail.com>
2022-12-17 15:50:52 +0500

    University initialised

---
+++ person.ts
@@ -0,0 +1,14 @@
+const interface NIC {
+    
+}
+class Person {
+    name:string,
+    ID:NIC,
+    
+
+    constructor(name:string , ID:number){
+        this.name = ""
+        this.ID = 0
+    }
+
+}
